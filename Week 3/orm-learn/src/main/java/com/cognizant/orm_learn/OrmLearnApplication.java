e39034535744fac7cfaa964ad85a489688604d7e
Saujanya Das <dassaujanya1911@gmail.com>
2025-07-05 15:32:39 +0530

    added the additional handson part

--- Week 3/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
+++ Week 3/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -1,37 +1,75 @@
 package com.cognizant.orm_learn;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.cognizant.orm_learn.model.Country;
-import com.cognizant.orm_learn.service.CountryService;
+import java.util.List;
 
 @SpringBootApplication
 public class OrmLearnApplication {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
 
     private static CountryService countryService;
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
-        LOGGER.info("Inside main");
-
         countryService = context.getBean(CountryService.class);
 
-        testGetAllCountries();
+        testGetCountryByCode();
+        testAddCountry();
+        testUpdateCountry();
+        testDeleteCountry();
+        testFindByPartialName();
     }
 
-    private static void testGetAllCountries() {
-        LOGGER.info("Start");
-        List<Country> countries = countryService.getAllCountries();
-        LOGGER.debug("countries={}", countries);
-        LOGGER.info("End");
+    private static void testGetCountryByCode() {
+        System.out.println("=== Get Country by Code 'IN' ===");
+        Country country = countryService.getCountryByCode("IN");
+        System.out.println(country);
+    }
+
+    private static void testAddCountry() {
+        System.out.println("=== Add Country 'XY' - 'Xylophone Land' ===");
+        Country newCountry = new Country();
+        newCountry.setCode("XY");
+        newCountry.setName("Xylophone Land");
+        try {
+            countryService.addCountry(newCountry);
+            System.out.println("Added: " + countryService.getCountryByCode("XY"));
+        } catch (Exception e) {
+            System.err.println("Error adding country: " + e.getMessage());
+        }
+    }
+
+    private static void testUpdateCountry() {
+        System.out.println("=== Update Country 'XY' name to 'Xylophone World' ===");
+        Country updatedCountry = new Country();
+        updatedCountry.setCode("XY");
+        updatedCountry.setName("Xylophone World");
+        try {
+            countryService.updateCountry(updatedCountry);
+            System.out.println("Updated: " + countryService.getCountryByCode("XY"));
+        } catch (Exception e) {
+            System.err.println("Error updating country: " + e.getMessage());
+        }
+    }
+
+    private static void testDeleteCountry() {
+        System.out.println("=== Delete Country with Code 'XY' ===");
+        try {
+            countryService.deleteCountry("XY");
+            System.out.println("Deleted country 'XY'");
+        } catch (Exception e) {
+            System.err.println("Error deleting country: " + e.getMessage());
+        }
+    }
+
+    private static void testFindByPartialName() {
+        System.out.println("=== Find countries containing 'an' ===");
+        List<Country> countries = countryService.findCountriesByPartialName("an");
+        countries.forEach(System.out::println);
     }
 }
--- Week 3/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
+++ Week 3/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -1,6 +1,7 @@
 package com.cognizant.orm_learn.service;
 
 import java.util.List;
+
 import jakarta.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,39 @@ public class CountryService {
     @Transactional
     public List<Country> getAllCountries() {
         return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country getCountryByCode(String code) {
+        return countryRepository.findById(code).orElse(null);
+    }
+
+    @Transactional
+    public Country addCountry(Country country) {
+        if (countryRepository.existsById(country.getCode())) {
+            throw new RuntimeException("Country with code " + country.getCode() + " already exists.");
+        }
+        return countryRepository.save(country);
+    }
+
+    @Transactional
+    public Country updateCountry(Country country) {
+        if (!countryRepository.existsById(country.getCode())) {
+            throw new RuntimeException("Country with code " + country.getCode() + " does not exist.");
+        }
+        return countryRepository.save(country);
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        if (!countryRepository.existsById(code)) {
+            throw new RuntimeException("Country with code " + code + " does not exist.");
+        }
+        countryRepository.deleteById(code);
+    }
+
+    @Transactional
+    public List<Country> findCountriesByPartialName(String partialName) {
+        return countryRepository.findByNameContainingIgnoreCase(partialName);
     }
 }
